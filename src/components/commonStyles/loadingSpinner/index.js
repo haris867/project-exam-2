e8f02b6b627b824820b1fc2aea2533891f098bef
Haris <haris.usman@hotmail.com>
2023-05-26 09:30:13 +0200

    Finished loading component

---
+++ src/components/commonStyles/loadingSpinner/index.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Spinner } from "react-bootstrap";
+
+export const LoadingSpinner = styled(Spinner)`
+  width: 100px;
+  height: 100px;
+  color: var(--color-primary);
+`;
