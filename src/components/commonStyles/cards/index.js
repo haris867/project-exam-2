7dbf7cf7a1da57add6cb4bb15a2eecd6c56d45c3
Haris <haris.usman@hotmail.com>
2023-05-26 09:29:40 +0200

    Finished card components

---
+++ src/components/commonStyles/cards/index.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export const BaseCard = styled(Card)`
+  background: none;
+  border: none;
+  width: 90%;
+  border-radius: 0%;
+  padding: 0;
+  margin-bottom: 40px;
+`;
+
+export const CardLink = styled(Link)`
+  color: var(--color-primary);
+  text-decoration: none;
+  :hover {
+    color: var(--color-primary);
+  }
+`;
+
+export const CardImg = styled(Card.Img)`
+  border-radius: 0;
+  border: solid 2px var(--color-tertiary);
+  aspect-ratio: 3/2;
+`;
