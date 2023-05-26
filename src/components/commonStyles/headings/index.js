0c8f1b97ec93c3f554b0e5ba6285f6e3eb6674e4
Haris <haris.usman@hotmail.com>
2023-05-26 09:29:57 +0200

    Finished heading components

---
+++ src/components/commonStyles/headings/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const MainHeadingContainer = styled.div`
+  width: 85%;
+  margin: 30px auto 0 auto;
+  font-weight: 100;
+`;
+
+export const MainHeading = styled.h1`
+  font-weight: 500;
+  margin-bottom: 40px;
+`;
