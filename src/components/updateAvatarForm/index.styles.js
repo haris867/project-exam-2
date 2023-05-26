097cd8429b34bf962be177a0f7fc7cfae6d083fb
Haris <haris.usman@hotmail.com>
2023-05-26 09:33:28 +0200

    Finished update form styled components

---
+++ src/components/updateAvatarForm/index.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { FormLabel, FormInput } from "../commonStyles/forms";
+
+export const UpdateFormLabel = styled(FormLabel)`
+  display: block;
+  color: var(--color-primary);
+`;
+
+export const UpdateFormInput = styled(FormInput)`
+flex: 1 0 100px,
+max-width: 350px,
+font-family: Quicksand,
+`;
