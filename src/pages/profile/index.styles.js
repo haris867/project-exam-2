0aa90d49cb1279ec31a19cdb96d144b165075cdd
Haris <haris.usman@hotmail.com>
2023-05-26 09:35:28 +0200

    Finished profile styled components

---
+++ src/pages/profile/index.styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { CgMathPlus } from "react-icons/cg";
+
+export const ProfileImage = styled.img`
+  border: solid 2px var(--color-tertiary);
+  min-width: 80px;
+  min-height: 80px;
+  max-width: 80px;
+  max-height: 80px;
+`;
+
+export const PlusIcon = styled(CgMathPlus)`
+  cursor: pointer;
+  color: var(--color-primary);
+  font-size: calc(2em + 0.8vw);
+`;
