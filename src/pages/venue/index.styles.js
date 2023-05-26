5e396153c4bd5afe44aeeac8090c0ac6c3c936dc
Haris <haris.usman@hotmail.com>
2023-05-26 09:36:06 +0200

    Finished venue styled components

---
+++ src/pages/venue/index.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const FacilityThumbnail = styled.div`
+  display: inline-block;
+  padding: 10px;
+  border: 3px solid var(--color-primary);
+  width: calc(40% + 2vw);
+  text-align: center;
+  background-color: white;
+  color: var(--color-primary);
+  font-weight: 700;
+`;
