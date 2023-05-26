153adca27c45ae4f9c2380538034a728f8a08f12
Haris <haris.usman@hotmail.com>
2023-05-26 09:29:49 +0200

    Finished form components

---
+++ src/components/commonStyles/forms/index.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const FormLabel = styled.label`
+  font-size: calc(1.2rem + 0.5vw) !important;
+`;
+export const FormInput = styled.input`
+  height: calc(2.5rem + 0.7vw) !important;
+`;
+export const FormCheckbox = styled.input`
+  width: calc(1.5rem + 0.7vw) !important;
+`;
+export const FormTextarea = styled.textarea`
+  height: calc(4.5rem + 0.7vw) !important;
+`;
+
+export const FormCheckboxContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+export const AddImageContainer = styled.div`
+  cursor: pointer;
+`;
