63f4e3a0bbe3481a4819c8222f8dfa50a3d0f7ef
Haris <haris.usman@hotmail.com>
2023-05-26 09:35:19 +0200

    Finished profile page

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,23 +1,48 @@
 import { React, useState } from "react";
 import Collapse from "react-bootstrap/Collapse";
 import { Container, Card, Row, Col } from "react-bootstrap";
-import { Route, NavLink } from "react-router-dom";
-import styled from "styled-components";
-import { useForm } from "react-hook-form";
-import { LogInButton } from "../../components/nav";
-import { BaseCard, CardLink, CardImg, CardTitle } from "../home";
-
-const LoginHeading = styled.h1`
-  font-weight: 500;
-  margin-bottom: 40px;
-`;
+import { useParams } from "react-router-dom";
+import * as St from "../../components/commonStyles/cards";
+import useGetData from "../../hooks/api/getData";
+import CreateVenueForm from "../../components/createVenueForm";
+import UpdateAvatarForm from "../../components/updateAvatarForm";
+import { load } from "../../storage";
+import { LoadingSpinner } from "../../components/commonStyles/loadingSpinner";
+import * as S from "./index.styles";
 
 export default function Profile() {
   const [open, setOpen] = useState(false);
+  const [openForm, setOpenForm] = useState(false);
+  const loggedInUser = load("user");
 
-  function onSubmit(data) {
-    console.log(data);
+  const url = "https://api.noroff.dev/api/v1/holidaze/profiles/";
+
+  let { name } = useParams();
+  const { data, isLoading, isError } = useGetData(
+    url + name + `?_bookings=true&_venues=true`,
+    {
+      headers: {
+        Authorization: `Bearer ${loggedInUser.accessToken}`,
+      },
+    }
+  );
+
+  console.log(data);
+
+  if (isLoading) {
+    return (
+      <LoadingSpinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </LoadingSpinner>
+    );
   }
+
+  if (isError) {
+    return <div>Error: {isError.message}</div>;
+  }
+
+  console.log(data.venues);
+
   return (
     <Container
       className="d-flex flex-column justify-content-center"
@@ -33,101 +58,134 @@ export default function Profile() {
             style={{ width: "95%" }}
           >
             <div className="d-flex flex-column align-items-center">
-              <img
-                src="/images/messi-profile.jpg"
-                alt=""
-                className="rounded-circle"
-                style={{ maxWidth: "80px" }}
-              />
-              <p
-                className="text-center"
-                style={{
-                  fontSize: "1em",
-                  color: "var(--color-primary)",
-                  textDecoration: "underline",
-                  cursor: "pointer",
-                }}
-                onClick={() => setOpen(!open)}
-                aria-controls="update-avatar-form"
-                aria-expanded={open}
-              >
-                Update avatar
-              </p>
+              {data.avatar ? (
+                <S.ProfileImage
+                  src={data.avatar}
+                  alt=""
+                  className="rounded-circle"
+                />
+              ) : (
+                <S.ProfileImage
+                  src="/images/messi-profile.jpg"
+                  alt=""
+                  className="rounded-circle"
+                />
+              )}
+              {loggedInUser.name === data.name ? (
+                <p
+                  className="text-center"
+                  style={{
+                    fontSize: "1em",
+                    color: "var(--color-primary)",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => setOpen(!open)}
+                  aria-controls="update-avatar-form"
+                  aria-expanded={open}
+                >
+                  Update avatar
+                </p>
+              ) : null}
             </div>
-            <h1 className="mb-5 ms-1 fw-normal fs-2">Lionel Messi</h1>
+            <h1 className="mb-5 fw-normal fs-2">{data.name}</h1>
           </Col>
           <Collapse in={open}>
             <Col id="update-avatar-form" xs={12}>
-              <div className="d-flex justify-content-center flex-wrap p-2 mb-3">
-                <input
-                  className="m-2"
-                  type="url"
-                  placeholder="Image URL"
-                  style={{
-                    height: "calc(2.5em + 0.7vw)",
-                    flex: "1 0 100px",
-                  }}
-                />
-                <LogInButton>UPDATE</LogInButton>
-              </div>
+              <UpdateAvatarForm />
             </Col>
           </Collapse>
           <Col className="d-flex align-items-center flex-column">
-            <h2
-              className="mb-3 fs-3"
-              style={{ width: "90%", color: "var(--color-primary)" }}
-            >
-              My bookings
-            </h2>
+            {data.venueManager ? (
+              <div
+                className="d-flex justify-content-between align-items-center mb-3"
+                style={{ width: "90%" }}
+              >
+                <h2
+                  className="fs-3 m-0"
+                  style={{ color: "var(--color-primary)" }}
+                >
+                  Venues
+                </h2>
+                {loggedInUser.name === data.name ? (
+                  <S.PlusIcon
+                    onClick={() => setOpenForm(!openForm)}
+                    aria-controls="create-venue-form"
+                    aria-expanded={open}
+                  />
+                ) : null}
+              </div>
+            ) : null}
+            {loggedInUser.name === data.name ? (
+              <Collapse in={openForm}>
+                <div id="create-venue-form">
+                  <CreateVenueForm />
+                </div>
+              </Collapse>
+            ) : null}
+            {data.venueManager && data.venues.length > 0 ? (
+              data.venues
+                .sort((a, b) => new Date(a.created) - new Date(b.created))
+                .reverse()
+                .map((venue) => (
+                  <St.BaseCard className="mb-3">
+                    <St.CardLink to={`/venue/${venue.id}`}>
+                      <Card.Text className="fs-4 mb-1">{venue.name}</Card.Text>
+                      <St.CardImg
+                        src={
+                          venue.media.length > 0
+                            ? venue.media[0]
+                            : "/images/beach-resort.jpg"
+                        }
+                        alt=""
+                      />
+                    </St.CardLink>
+                  </St.BaseCard>
+                ))
+            ) : (
+              <div>You have no venues</div>
+            )}
+            {loggedInUser.name === data.name ? (
+              <div>
+                <h2
+                  className="mb-3 fs-3"
+                  style={{ width: "90%", color: "var(--color-primary)" }}
+                >
+                  Bookings
+                </h2>
+                {data.bookings && data.bookings.length > 0 ? (
+                  data.bookings.map((booking) => (
+                    <St.BaseCard className="mb-3">
+                      <St.CardLink to={`/venue/${booking.venue.id}`}>
+                        <Card.Text className="fs-4 mb-1">
+                          {booking.venue.name}
+                        </Card.Text>
+                        <St.CardImg
+                          src={
+                            booking.venue.media.length > 0
+                              ? booking.venue.media[0]
+                              : "/images/beach-resort.jpg"
+                          }
+                          alt=""
+                        />
 
-            <BaseCard className="mb-3">
-              <CardLink to={`/register`}>
-                <Card.Text className="fs-4 mb-1">Beach Resort</Card.Text>
-                <CardImg src="/images/beach-resort.jpg" alt="" />
-                <div
-                  className="d-flex justify-content-between mt-2"
-                  style={{ alignItems: "baseline" }}
-                >
-                  <Card.Text className="fs-6">23.05.23-30.05.23</Card.Text>
-                </div>
-              </CardLink>
-            </BaseCard>
-            <BaseCard className="mb-3">
-              <CardLink to={`/register`}>
-                <Card.Text className="fs-4 mb-1">City Suite</Card.Text>
-                <CardImg src="/images/city-suite.jpg" alt="" />
-                <div
-                  className="d-flex justify-content-between mt-2"
-                  style={{ alignItems: "baseline" }}
-                >
-                  <Card.Text className="fs-6">23.05.23-30.05.23</Card.Text>
-                </div>
-              </CardLink>
-            </BaseCard>
-            <BaseCard className="mb-3">
-              <CardLink to={`/register`}>
-                <Card.Text className="fs-4 mb-1">Beach Resort</Card.Text>
-                <CardImg src="/images/beach-resort.jpg" alt="" />
-                <div
-                  className="d-flex justify-content-between mt-2"
-                  style={{ alignItems: "baseline" }}
-                >
-                  <Card.Text className="fs-6">23.05.23-30.05.23</Card.Text>
-                </div>
-              </CardLink>
-            </BaseCard>
-            <BaseCard className="mb-3">
-              <CardLink to={`/register`}>
-                <Card.Text className="fs-4 mb-1">City Suite</Card.Text>
-                <CardImg src="/images/city-suite.jpg" alt="" />
-                <div
-                  className="d-flex justify-content-between mt-2"
-                  style={{ alignItems: "baseline" }}
-                >
-                  <Card.Text className="fs-6">23.05.23-30.05.23</Card.Text>
-                </div>
-              </CardLink>
-            </BaseCard>
+                        <div className="d-flex justify-content-between mt-2">
+                          <Card.Text className="fs-6">
+                            {new Date(booking.dateFrom).toLocaleDateString()} -
+                            {new Date(booking.dateTo).toLocaleDateString()}
+                          </Card.Text>
+                          <Card.Text className="fs-6">
+                            Number of guests: {booking.guests}
+                          </Card.Text>
+                        </div>
+                      </St.CardLink>
+                    </St.BaseCard>
+                  ))
+                ) : (
+                  <div>You have no bookings</div>
+                )}
+              </div>
+            ) : null}
           </Col>
         </Col>
       </Row>
