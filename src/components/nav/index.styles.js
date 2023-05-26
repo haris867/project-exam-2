2ed9c500b093ec49425a258eaa2504dfa4faa3cb
Haris <haris.usman@hotmail.com>
2023-05-26 09:32:05 +0200

    Finished nav components

---
+++ src/components/nav/index.styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const Nav = styled.nav`
+  position: absolute;
+  right: 25px;
+  top: 117px;
+  font-family: "Roboto", sans-serif;
+  z-index: 1;
+  background: var(--color-secondary);
+  text-align: center;
+  border: 3px solid var(--color-primary);
+`;
+
+export const NavList = styled.ul`
+  list-style-type: none;
+  padding: 10px 13px;
+  text-align: center;
+  margin-bottom: 0;
+`;
+
+export const NavBarLink = styled(NavLink)`
+  text-decoration: none;
+  margin: 0 10px;
+  color: var(--color-primary);
+  :hover {
+    color: var(--color-primary);
+    text-decoration: underline;
+  }
+`;
